5c0951b7d5f81cd9dc64a3e78637512f55c905eb
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-23 14:14:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+function titleCased(){
+  return tutorials.map(function (tutorial) {
+    let strings = tutorial.split(" ");
+    for (let i = 0; i <strings.length; i++){
+      strings[i] = strings[i][0].toUpperCase() + strings[i].slice(1);
+    }
+    return strings.join(" ");
+  })
 
-const titleCased = () => {
-  return tutorials
 }
